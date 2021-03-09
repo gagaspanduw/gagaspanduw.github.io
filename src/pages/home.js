f3ef53d88e4516297a57b4d96e70bc727dd4bcc0
Gagas PW <nothaveid082@gmail.com>
2021-03-09 15:43:00 +0700

    add hover mail me

--- src/pages/home.js
+++ src/pages/home.js
@@ -33,7 +33,7 @@ const Home = (props) => (
                     </div>
                     <div className="flex flex-row items-center pt-5">
                         <div className="pr-5">
-                            <button className="bg-red-600 p-2 text-sm rounded flex flex-row"><svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <button className="hover:bg-red-800 bg-red-600 p-2 text-sm rounded flex flex-row"><svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg> &nbsp; Mail me </button>
                         </div>
