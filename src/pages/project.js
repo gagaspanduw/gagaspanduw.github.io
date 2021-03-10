4c1c3b88527f65e491a27b029f85ede9dca9a89f
Gagas PW <nothaveid082@gmail.com>
2021-03-10 17:21:02 +0700

    edit link and hover

--- src/pages/project.js
+++ src/pages/project.js
@@ -12,36 +12,42 @@ const Project = (props) => (
             <h2 className="pb-5 text-1xl font-bold">Website Developing</h2>
             <div className="flex flex-col justify-center lg:flex-row">
                 <div className="flex flex-col">
-                    <img className="w-4/4 lg:w-3/4" src={owntols}></img>
-                    <a href="https://www.owntols.com" className="text-sm pt-5 pb-20 flex flex-row">
-                    <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-                    </svg>&nbsp;View website</a>
+                    <a target="_blank" href="https://www.owntols.com"><img className="w-4/4 lg:w-3/4" src={owntols}></img></a>
+                    <div className="text-sm pt-5 pb-20 flex flex-row">
+                        <a target="_blank" href="https://www.owntols.com" className="flex flex-row">
+                        <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                        </svg>&nbsp;View website</a>
+                    </div>
                 </div>
                 <div className="flex flex-col justify-center">
-                    <img className="w-4/4 lg:w-3/4" src={nubie}></img>
-                    <a href="https://www.nubie.id" className="text-sm pt-5 pb-20 flex flex-row">
-                    <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-                    </svg>&nbsp;View website</a>
+                    <a target="_blank" href="https://www.nubie.id"><img className="w-4/4 lg:w-3/4" src={nubie}></img></a>
+                    <div className="text-sm pt-5 pb-20 flex flex-row">
+                        <a target="_blank" href="https://www.nubie.id" className="flex flex-row">
+                        <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                        </svg>&nbsp;View website</a>
+                    </div>
                 </div>
             </div>
 
             <h2 className="pb-5 text-1xl font-bold">UI/UX Design</h2>
             <div className="flex flex-col lg:flex-row">
                 <div className="flex flex-col justify-center">
-                    <img className="w-4/4 lg:w-3/4 transition ease-in-out duration-700 transform hover:scale-150" src={uiuxwebsite}></img>
+                <a target="_blank" href="https://www.instagram.com/gagaspanduw"><img className="w-4/4 lg:w-3/4 transition ease-in-out duration-700 transform hover:scale-150" src={uiuxwebsite}></img></a>
                     <a href="https://www.instagram.com/gagaspanduw" className="text-sm pt-5 pb-20 flex flex-row">
                     <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
                     </svg>&nbsp;View details</a>
                 </div>
                 <div className="flex flex-col justify-center">
-                    <img className="w-4/4 lg:w-3/4 transition ease-in-out duration-700 transform hover:scale-150" src={uiuxapp}></img>
-                    <a href="https://www.instagram.com/gagaspanduw" className="text-sm pt-5 pb-20 flex flex-row">
-                    <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-                    </svg>&nbsp;View details</a>
+                    <a target="_blank" href="https://www.instagram.com/gagaspanduw"><img className="w-4/4 lg:w-3/4 transition ease-in-out duration-700 transform hover:scale-150" src={uiuxapp}></img></a>
+                    <div className="text-sm pt-5 pb-20 flex flex-row">
+                        <a target="_blank" href="https://www.instagram.com/gagaspanduw" className="flex flex-row">
+                        <svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                        </svg>&nbsp;View details</a>
+                    </div>
                 </div>
             </div>
 
