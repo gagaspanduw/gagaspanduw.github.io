47198bc8afe384726c3f94a91e3d283cb8d8e65a
Gagas Pandu W <gagaspanduw@gmail.com>
2022-10-24 21:15:00 +0700

    remove cv download and edit name

--- src/pages/home.js
+++ src/pages/home.js
@@ -23,7 +23,7 @@ const Home = (props) => (
         <div className="lg:pl-32 lg:pr-32 p-10 text-left">
             <div className="flex flex-1 items-center">
                 <div>
-                    <h3 className="text-red-600 text-3xl font-bold">Hello, I'm Gagas Pandu W</h3>
+                    <h3 className="text-red-600 text-3xl font-bold">Hello, I'm Gagas</h3>
                     <h2 className="font-bold text-5xl pt-1 pb-1">Software Engineer Focusing on Software Testing</h2>
                     <p className="text-sm pt-6 pb-6">I am from Indonesia, i work as Software Quality Assurance who have
                     responsibility to make test case, do manual test, and make automation testing.   
@@ -39,16 +39,11 @@ const Home = (props) => (
                         <a className="p-4" target="_blank" href="https://github.com/gagaspanduw">
                         <svg className="fill-current text-white hover:text-gray-400" xmlns="http://www.w3.org/2000/svg" width="26" viewBox="0 0 24 24"><title>Github Icon</title><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
                         </a>
-                    </div>
-                    <div className="flex flex-row items-center pt-5">
-                        <div className="pr-5">
+                        <div className="pl-4 pt-3">
                             <a href="mailto:gagaspanduw@gmail.com" className="hover:bg-red-800 bg-red-600 p-2 text-sm rounded flex flex-row"><svg width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg> &nbsp; Mail me </a>
                         </div>
-                        <a className="hover:text-gray-400 text-sm ml-2 flex flex-row" href="https://drive.google.com/file/d/148Wxv5lOffhVm9k4ANt2qOuTS23V1i8N/view?usp=sharing"><svg className="animate-bounce" width="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
-                        </svg> &nbsp;Download CV</a>
                     </div>
                 </div>
 
